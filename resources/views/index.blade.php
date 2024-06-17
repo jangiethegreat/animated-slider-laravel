61ab81f6f182b9ef68cffdc5015d8769b211170f
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2024-06-17 14:30:55 +0800

    animated slider not done

---
+++ resources/views/index.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="carousel">
+    <div class="list">
+        <div class="item">
+            <img src="{{ asset('images/1.jpg') }}">
+            <div class="content">
+                <div class="author">JANG</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">PHILIPPINES</div>
+                <div class="des">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In consequuntur error nulla asperiores consectetur! Quaerat illum recusandae ipsam animi et mollitia rerum ut repudiandae earum, reiciendis saepe ullam expedita dicta!
+                </div>
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                    <button>SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="{{ asset('images/2.jpg') }}">
+            <div class="content">
+                <div class="author">JANG</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">PHILIPPINES</div>
+                <div class="des">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In consequuntur error nulla asperiores consectetur! Quaerat illum recusandae ipsam animi et mollitia rerum ut repudiandae earum, reiciendis saepe ullam expedita dicta!
+                </div>
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                    <button>SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="{{ asset('images/3.jpg') }}">
+            <div class="content">
+                <div class="author">JANG</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">PHILIPPINES</div>
+                <div class="des">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In consequuntur error nulla asperiores consectetur! Quaerat illum recusandae ipsam animi et mollitia rerum ut repudiandae earum, reiciendis saepe ullam expedita dicta!
+                </div>
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                    <button>SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="{{ asset('images/4.jpg') }}">
+            <div class="content">
+                <div class="author">JANG</div>
+                <div class="title">DESIGN SLIDER</div>
+                <div class="topic">PHILIPPINES</div>
+                <div class="des">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In consequuntur error nulla asperiores consectetur! Quaerat illum recusandae ipsam animi et mollitia rerum ut repudiandae earum, reiciendis saepe ullam expedita dicta!
+                </div>
+                <div class="buttons">
+                    <button>SEE MORE</button>
+                    <button>SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animated Slider</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <a href=""> Home</a>
+            <a href=""> Contacts</a>
+            <a href=""> Info</a>
+        </nav>
+    </header>
+
+    <script src="{{ asset('app.js') }}"></script>
+    @yield('content')
+</body>
+
+</html>
